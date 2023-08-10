1a505b190e586238a74f7a0c1c9d7eacf0e4901d
Richard Austin <richardaustin123@icloud.com>
2023-08-10 17:32:28 +0100

    API get artist info

---
+++ src/components/Artist.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import { Button } from 'react-bootstrap'
+import { useState, useEffect } from 'react'
+import config from '../config.json'
+
+//bootstrap
+import Form from 'react-bootstrap/Form'
+
+// TEST ID: 1Ffb6ejR6Fe5IamqA5oRUF, 630wzNP2OL7fl4Xl0GnMWq
+
+function Artist() {
+
+  // Information
+  let endpoint = 'https://api.spotify.com/v1/artists/'
+  
+  let [artist, setArtist] = useState('')
+  let [artistInfo, setArtistInfo] = useState([])
+
+  function getArtist() {
+    // Get the artist from spotify API
+    fetch(endpoint + artist, {
+      method: 'GET',
+      headers: {
+        'Authorization': 'Bearer ' + config.access_token
+      }
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data)
+      setArtistInfo(data)
+    })
+    .catch(error => {
+      console.log(error)
+    })
+
+  }
+
+  return (
+    <div>
+
+      <h1>Search Artist</h1>
+      <Form.Control type="text" placeholder="Search Artist" onChange={(e) => setArtist(e.target.value)} />
+      <Button onClick={getArtist}>Click me</Button>
+
+      {artistInfo.length !== 0 &&
+        <div>
+          <h1>{artistInfo.name}</h1>
+          <img src={artistInfo.images[1].url} alt="artist" />
+          <p>Followers: {artistInfo.followers.total}</p>
+          <p>Popularity: {artistInfo.popularity}</p>
+          <p>Genres:</p>
+          {artistInfo.genres.map((genre, index) => {
+            return <p key={index}>{genre}</p>
+          }
+          )}
+        </div>
+      }
+
+    </div>
+  )
+}
+
+export default Artist
+
